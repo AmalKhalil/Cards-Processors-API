e96bbe483d9a6fc3895cbaef446206472b23f2d7
Amal Khalil <Amal.Khalil@itworx.com>
2018-06-01 13:45:57 +0200

    Remove Scripts as it was replaced by liquibase

--- scripts/Users.sql
+++
@@ -1,2 +0,0 @@
-insert into card_processors.user(user_name,password,role) values ("Merchant1","password",0);
-insert into card_processors.user(user_name,password,role) values ("Merchant2","password",0);
